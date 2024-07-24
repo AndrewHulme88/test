c91809d31b173f49a933b967dd4b3d73f033a38a
andrew hulme <andrew_hulme04@hotmail.com>
2024-07-24 16:01:42 +1000

    Commit

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
-import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')  # or 'Qt5Agg' for PyQt5, etc.
+import matplotlib.pyplot as plt
 
-# Sample DataFrame df1
-df1 = pd.DataFrame({
-    'Group': ['A', 'B', 'A', 'B', 'A', 'B'],
-    'Value': [10, 20, 30, 40, 50, 60]
-})
-
-# Sample DataFrame df2 with the same groups
-df2 = pd.DataFrame({
-    'Group': ['A', 'B', 'A', 'B', 'A', 'B'],
-    'OtherValue': [1, 2, 3, 4, 5, 6]
-})
-
-# Compute the mean value for each group in df1
-mean_values = df1.groupby('Group')['Value'].mean()
-
-# Map the mean values to df2 based on the group
-df2['MeanValue'] = df2['Group'].map(mean_values)
-
-print(df2)
+# Your plotting code here
+plt.plot(range(10), range(10))
+plt.show()
