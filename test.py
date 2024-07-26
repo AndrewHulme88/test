4e6f9676264052723bbdf65b693f0b472bf37c38
andrew hulme <andrew_hulme04@hotmail.com>
2024-07-26 13:05:05 +1000

    Commit

--- test.py
+++ test.py
@@ -1,31 +1,37 @@
 import matplotlib.pyplot as plt
 
-def top_8_game_sales(game_data):
-    # Extract game names and sales from the input dictionary
+def top_8_game_sales(game_data, graph_type):
     games = list(game_data.keys())
     sales = list(game_data.values())
 
-    # Plot a bar graph using Matplotlib
-    plt.bar(games, sales, color='green', width=0.8)
+    if graph_type == 'bar':
+        plt.bar(games, sales, color='green', width=0.8)
+    elif graph_type == 'line':
+        plt.plot(games, sales, color='green', marker='o', linestyle='-')
+    elif graph_type == 'pie':
+        plt.pie(sales, labels=games, autopct='%1.1f%%')
+    else:
+        print("Invalid graph type chosen. Please choose from 'bar', 'line', or 'pie'.")
+        return
 
-    # Set the plot title and axis labels
     plt.title("Top 8 Best-Selling Video Games")
-    plt.xlabel("Game")
-    plt.ylabel("Sales (in millions)")
-
-    # Show the grid lines and display the plot
-    plt.grid(axis='y', color='gray', linestyle='--')
+    if graph_type != 'pie':
+        plt.xlabel("Game")
+        plt.ylabel("Sales (in millions)")
     plt.show()
 
 game_data = {
-        "The Elder Scrolls V: Skyrim": 60,
-        "Red Dead Redemption 2": 64,
-        "Mario Kart 8 Deluxe" : 70.43,
-        "PUBG": 75,
-        "Wii Sports": 82.9,
-        "Grand Theft Auto V": 200,
-        "Minecraft": 300,
-        "Tetris": 520
-        }
+    "The Elder Scrolls V: Skyrim": 60,
+    "Red Dead Redemption 2": 64,
+    "Mario Kart 8 Deluxe": 70.43,
+    "PUBG": 75,  "Wii Sports": 82.9,  "Grand Theft Auto V": 200,  "Minecraft":300, "Tetris": 520
+ }
 
-top_8_game_sales(game_data)
+graph_types = ['bar', 'line', 'pie']
+print("Available Graph Types:", graph_types)
+choice = input("Choose the type of graph (bar, line, pie): ").lower()
+
+while choice not in graph_types:
+    choice = input("Invalid choice. Please choose from 'bar', 'line', or 'pie': ").lower()
+
+top_8_game_sales(game_data, choice)
